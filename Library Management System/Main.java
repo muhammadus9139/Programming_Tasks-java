a0f2ca4d0161f7a3ffa41ef5a22d1bf1c6c3d31c
Muhammad Usama <muhammadus9139@gmail.com>
2026-03-17 10:38:40 +0500

    Add files via upload

--- Library Management System/Main.java
+++ Library Management System/Main.java
@@ -1,25 +1,16 @@
 public class Main{
-    public static void main(String args[]){
+   public static void main(String args[]){
+     //Book b1=new Book("Civics","usama",Type.HISTORY,true);
+     //System.out.println(b1);
 
 
-        Game game1= new Game(3);
+     //Shelf s1=new Shelf("Shelf 1");
+     //Book b =s1.findbook("Book 4");
+     //System.out.println(b);
 
-        game1.distribute();
-
-        while(Game.counter < 5)
-            game1.playhand();
-
-        game1.getvictor();
-
-        //Card c1=new Card(Suit.SPADES,Rank.ACE);
-        //System.out.println("Before Shuffle");
-        //System.out.println(c1.toString());
-        //Deck deck=new Deck();
-        //deck.display();
-
-        //System.out.println("================");
-        //deck.Shuffle();
-        //deck.display();
-
+     Section s2=new Section("Section 1",2,5);
+     //System.out.println(s2);
+     Book b2 =s2.findbook(2,4,"Book 1");
+     System.out.println(b2);
     }
 }
